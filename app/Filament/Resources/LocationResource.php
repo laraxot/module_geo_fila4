c12f8c65ea1b012821eb3b47bcd5ec7417715548
marco76tv <marco.sottana@gmail.com>
2025-09-24 08:44:25 +0200

    .

--- app/Filament/Resources/LocationResource.php
+++ app/Filament/Resources/LocationResource.php
@@ -39,6 +39,7 @@ class LocationResource extends XotBaseResource
     public static function getFormSchema(): array
     {
         return [
+<<<<<<< HEAD
             'name' => TextInput::make('name')->required()->maxLength(255),
             'latitude' => TextInput::make('latitude')->required()->numeric(),
             'longitude' => TextInput::make('longitude')->required()->numeric(),
@@ -47,6 +48,16 @@ class LocationResource extends XotBaseResource
             'state' => TextInput::make('state')->maxLength(255),
             'zip' => TextInput::make('zip')->maxLength(255),
             'formatted_address' => TextInput::make('formatted_address')->maxLength(1024),
+=======
+            TextInput::make('name')->required()->maxLength(255),
+            TextInput::make('latitude')->required()->numeric(),
+            TextInput::make('longitude')->required()->numeric(),
+            TextInput::make('street')->maxLength(255),
+            TextInput::make('city')->maxLength(255),
+            TextInput::make('state')->maxLength(255),
+            TextInput::make('zip')->maxLength(255),
+            TextInput::make('formatted_address')->maxLength(1024),
+>>>>>>> 535c490 (.)
             // Temporaneamente commentato per compatibilità Filament 4.x
             // Map::make('location')
             //     ->reactive()
