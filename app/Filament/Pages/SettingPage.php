6f065900d959e27e85c7328810c16e4711617c9e
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:26:02 +0100

    .

--- app/Filament/Pages/SettingPage.php
+++ app/Filament/Pages/SettingPage.php
@@ -20,7 +20,7 @@ class SettingPage extends XotBasePage
         ];
 
         return [
-            EnvWidget::make(['only' => $only]),
+            'env' => EnvWidget::make(['only' => $only]),
         ];
     }
 }
