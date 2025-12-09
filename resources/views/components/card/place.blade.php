59d65770ae37772a7d550d18cdbbde959f662773
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:26:58 +0100

    .

--- resources/views/components/card/place.blade.php
+++ resources/views/components/card/place.blade.php
@@ -1,8 +1,26 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> f0b4f5c (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> f0b4f5c (.)
 <div class="b-pop-places__item">
     <div class="b-pop-place">
         <div class="b-pop-place__img">
