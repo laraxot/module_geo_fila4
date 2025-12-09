19d77c2029b493005e61a94443c49bb37217a53a
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:26:22 +0100

    .

--- app/Transformers/GeoJsonCollection.php
+++ app/Transformers/GeoJsonCollection.php
@@ -5,9 +5,33 @@ declare(strict_types=1);
 namespace Modules\Geo\Transformers;
 
 /*
+<<<<<<< HEAD
  *  GEOJSON e' uno standard
  * https://it.wikipedia.org/wiki/GeoJSON
  */
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+ *  GEOJSON e' uno standard
+ * https://it.wikipedia.org/wiki/GeoJSON
+ */
+=======
+*  GEOJSON e' uno standard
+* https://it.wikipedia.org/wiki/GeoJSON
+*/
+>>>>>>> a12f125f4a (.)
+=======
+ *  GEOJSON e' uno standard
+ * https://it.wikipedia.org/wiki/GeoJSON
+ */
+>>>>>>> b93ef594b4 (.)
+=======
+*  GEOJSON e' uno standard
+* https://it.wikipedia.org/wiki/GeoJSON
+*/
+>>>>>>> origin/develop
+>>>>>>> f0b4f5c (.)
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
@@ -32,8 +56,28 @@ class GeoJsonCollection extends ResourceCollection
             'type' => 'FeatureCollection',
             'features' => $this->collection,
             /*'links' => [
+<<<<<<< HEAD
              * 'self' => 'link-value',
              * ],*/
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+             * 'self' => 'link-value',
+             * ],*/
+=======
+                'self' => 'link-value',
+            ],*/
+>>>>>>> a12f125f4a (.)
+=======
+             * 'self' => 'link-value',
+             * ],*/
+>>>>>>> b93ef594b4 (.)
+=======
+                'self' => 'link-value',
+            ],*/
+>>>>>>> origin/develop
+>>>>>>> f0b4f5c (.)
         ];
     }
 }
--- resources/views/filament/widgets/location.blade.php
+++ resources/views/filament/widgets/location.blade.php
@@ -1,8 +1,26 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> f0b4f5c (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> f0b4f5c (.)
 <x-filament-widgets::widget>
     <x-filament::section>
         <form wire:submit="submit">
@@ -18,4 +36,20 @@ declare(strict_types=1);
             </div>
         </form>
     </x-filament::section>
+<<<<<<< HEAD
 </x-filament-widgets::widget>
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+</x-filament-widgets::widget>
+=======
+</x-filament-widgets::widget> 
+>>>>>>> a12f125f4a (.)
+=======
+</x-filament-widgets::widget>
+>>>>>>> b93ef594b4 (.)
+=======
+</x-filament-widgets::widget> 
+>>>>>>> origin/develop
+>>>>>>> f0b4f5c (.)
