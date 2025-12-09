ba829908dc1b1dc46c5c68eb2b9268f28f2b6ac1
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:26:25 +0100

    .

--- resources/views/filament/widgets/lat-lng.blade.php
+++ resources/views/filament/widgets/lat-lng.blade.php
@@ -1,28 +1,8 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> f0b4f5c (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
-
->>>>>>> origin/develop
->>>>>>> f0b4f5c (.)
 <x-filament-widgets::widget >
     Lat-lng <br/>
     lat:{{ $lat }}<br/>
