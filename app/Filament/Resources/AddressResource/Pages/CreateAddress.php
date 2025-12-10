c0b113a9efaa8d89bf010a524d6c6ed59e71f5ae
marco76tv <marco.sottana@gmail.com>
2025-12-10 11:13:25 +0100

    .

--- app/Filament/Resources/AddressResource/Pages/CreateAddress.php
+++ app/Filament/Resources/AddressResource/Pages/CreateAddress.php
@@ -4,48 +4,10 @@ declare(strict_types=1);
 
 namespace Modules\Geo\Filament\Resources\AddressResource\Pages;
 
-<<<<<<< HEAD
 use Modules\Geo\Filament\Resources\AddressResource;
 use Modules\Xot\Filament\Resources\Pages\XotBaseCreateRecord;
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-use Modules\Geo\Filament\Resources\AddressResource;
-use Modules\Xot\Filament\Resources\Pages\XotBaseCreateRecord;
-=======
-use Modules\Xot\Filament\Resources\Pages\XotBaseCreateRecord;
-use Modules\Geo\Filament\Resources\AddressResource;
-
->>>>>>> a12f125f4a (.)
-=======
-use Modules\Geo\Filament\Resources\AddressResource;
-use Modules\Xot\Filament\Resources\Pages\XotBaseCreateRecord;
->>>>>>> b93ef594b4 (.)
-=======
-use Modules\Xot\Filament\Resources\Pages\XotBaseCreateRecord;
-use Modules\Geo\Filament\Resources\AddressResource;
-
->>>>>>> origin/develop
->>>>>>> f0b4f5c (.)
 
 class CreateAddress extends XotBaseCreateRecord
 {
     protected static string $resource = AddressResource::class;
-<<<<<<< HEAD
 }
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-}
-=======
-}
->>>>>>> a12f125f4a (.)
-=======
-}
->>>>>>> b93ef594b4 (.)
-=======
-}
->>>>>>> origin/develop
->>>>>>> f0b4f5c (.)
