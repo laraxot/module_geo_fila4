664ecb443a9c708e2d5866d8d480925bcfb5e9e8
marco76tv <marco.sottana@gmail.com>
2025-12-10 11:12:27 +0100

    .

--- resources/views/components/blocks/map/location-map-table.blade.php
+++ resources/views/components/blocks/map/location-map-table.blade.php
@@ -1,15 +1,22 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> c942565 (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
 =======
 >>>>>>> bc26394 (.)
+=======
+>>>>>>> c942565 (.)
 <div>
     {{--
     @livewire(\Modules\Geo\Filament\Widgets\LocationMapWidget::class)
     --}}
+<<<<<<< HEAD
 <<<<<<< HEAD
     {{-- TEMPORANEAMENTE DISABILITATO PER COMPATIBILITÀ FILAMENT V4
     @livewire(\Modules\Geo\Filament\Widgets\LocationMapTableWidget::class)
@@ -17,6 +24,11 @@ declare(strict_types=1);
 =======
     @livewire(\Modules\Geo\Filament\Widgets\LocationMapTableWidget::class)
 >>>>>>> bc26394 (.)
+=======
+    {{-- TEMPORANEAMENTE DISABILITATO PER COMPATIBILITÀ FILAMENT V4
+    @livewire(\Modules\Geo\Filament\Widgets\LocationMapTableWidget::class)
+    --}}
+>>>>>>> c942565 (.)
     {{--
     @livewire(\Modules\Blog\Filament\Widgets\SampleChartWidget::class)
     --}}
