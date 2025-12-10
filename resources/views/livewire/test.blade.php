7dfc32a1952bdbdc3c8bbbf9b9cc895a58cf7535
marco76tv <marco.sottana@gmail.com>
2025-12-10 11:11:14 +0100

    .

--- resources/views/livewire/test.blade.php
+++ resources/views/livewire/test.blade.php
@@ -1,26 +1,8 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> f0b4f5c (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> f0b4f5c (.)
 <div>
 {{ Theme::add('https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js') }}
     {{ __FILE__ }}{{ __LINE__ }}
@@ -35,20 +17,4 @@ declare(strict_types=1);
             wire:submit.prevent="createAddress">{{__('Add Address')}}</button>
     </div>
     </form>
-<<<<<<< HEAD
 </div>
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-</div>
-=======
-</div>
->>>>>>> a12f125f4a (.)
-=======
-</div>
->>>>>>> b93ef594b4 (.)
-=======
-</div>
->>>>>>> origin/develop
->>>>>>> f0b4f5c (.)
