95bab12bba68efc4fcf300a2f5c57f20374f0208
marco76tv <marco.sottana@gmail.com>
2025-12-11 15:27:21 +0100

    .

--- app/Actions/ValidateCoordinatesAction.php
+++ app/Actions/ValidateCoordinatesAction.php
@@ -8,25 +8,7 @@ class ValidateCoordinatesAction
 {
     public function execute(float $latitude, float $longitude): bool
     {
-<<<<<<< HEAD
         return $this->isValidLatitude($latitude) && $this->isValidLongitude($longitude);
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-        return $this->isValidLatitude($latitude) && $this->isValidLongitude($longitude);
-=======
-        return $this->isValidLatitude($latitude)
-               && $this->isValidLongitude($longitude);
->>>>>>> a12f125f4a (.)
-=======
-        return $this->isValidLatitude($latitude) && $this->isValidLongitude($longitude);
->>>>>>> b93ef594b4 (.)
-=======
-        return $this->isValidLatitude($latitude)
-               && $this->isValidLongitude($longitude);
->>>>>>> origin/develop
->>>>>>> f0b4f5c (.)
     }
 
     private function isValidLatitude(float $latitude): bool
