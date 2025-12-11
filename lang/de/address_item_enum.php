0d507a4c0a25362d87cdb7ba620be060913d30f3
marco76tv <marco.sottana@gmail.com>
2025-12-11 17:43:47 +0100

    .

---
+++ lang/de/address_item_enum.php
@@ -0,0 +1,94 @@
+<?php
+
+return [
+    'phone' => [
+        'label' => 'Telefon',
+        'description' => 'Telefonnummer',
+        'icon' => 'heroicon-o-phone',
+        'color' => 'primary',
+    ],
+    'name' => [
+        'label' => 'Name',
+        'description' => 'Name des Ortes oder der Adresse',
+        'icon' => 'heroicon-o-tag',
+        'color' => 'secondary',
+    ],
+    'description' => [
+        'label' => 'Beschreibung',
+        'description' => 'Detaillierte Ortsbeschreibung',
+        'icon' => 'heroicon-o-document-text',
+        'color' => 'gray',
+    ],
+    'route' => [
+        'label' => 'Straße',
+        'description' => 'Straßen- oder Wegname',
+        'icon' => 'heroicon-o-map',
+        'color' => 'success',
+    ],
+    'street_number' => [
+        'label' => 'Hausnummer',
+        'description' => 'Hausnummer der Adresse',
+        'icon' => 'heroicon-o-home',
+        'color' => 'info',
+    ],
+    'locality' => [
+        'label' => 'Ortsteil',
+        'description' => 'Ortsteil oder Weiler',
+        'icon' => 'heroicon-o-building-office-2',
+        'color' => 'warning',
+    ],
+    'administrative_area_level_3' => [
+        'label' => 'Gemeinde',
+        'description' => 'Zugehörige Gemeinde',
+        'icon' => 'heroicon-o-building-office',
+        'color' => 'primary',
+    ],
+    'administrative_area_level_2' => [
+        'label' => 'Provinz',
+        'description' => 'Zugehörige Provinz',
+        'icon' => 'heroicon-o-map-pin',
+        'color' => 'danger',
+    ],
+    'administrative_area_level_1' => [
+        'label' => 'Region',
+        'description' => 'Zugehörige Region',
+        'icon' => 'heroicon-o-globe-alt',
+        'color' => 'purple',
+    ],
+    'country' => [
+        'label' => 'Land',
+        'description' => 'Staat oder Nation',
+        'icon' => 'heroicon-o-flag',
+        'color' => 'success',
+    ],
+    'postal_code' => [
+        'label' => 'Postleitzahl',
+        'description' => 'Postleitzahl',
+        'icon' => 'heroicon-o-inbox',
+        'color' => 'warning',
+    ],
+    'formatted_address' => [
+        'label' => 'Formatierte Adresse',
+        'description' => 'Vollständige formatierte Adresse',
+        'icon' => 'heroicon-o-document-text',
+        'color' => 'gray',
+    ],
+    'place_id' => [
+        'label' => 'Orts-ID',
+        'description' => 'Eindeutige Ortskennung',
+        'icon' => 'heroicon-o-finger-print',
+        'color' => 'info',
+    ],
+    'latitude' => [
+        'label' => 'Breitengrad',
+        'description' => 'Geografische Breitengrad-Koordinaten',
+        'icon' => 'heroicon-o-map-pin',
+        'color' => 'primary',
+    ],
+    'longitude' => [
+        'label' => 'Längengrad',
+        'description' => 'Geografische Längengrad-Koordinaten',
+        'icon' => 'heroicon-o-map-pin',
+        'color' => 'primary',
+    ],
+];
