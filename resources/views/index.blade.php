e2ba474ea0640075c1bcad33818bb21fb6da3297
marco76tv <marco.sottana@gmail.com>
2025-12-11 15:28:08 +0100

    .

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,26 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> f0b4f5c (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> f0b4f5c (.)
 @extends('geo::layouts.master')
 
 @section('content')
