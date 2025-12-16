bdbf72f6fda04118d9d3395d25680ce3e82f5df6
marco76tv <marco.sottana@gmail.com>
2025-12-16 19:38:47 +0100

    .

--- app/Filament/Pages/DotswanMap.php
+++ app/Filament/Pages/DotswanMap.php
@@ -36,9 +36,6 @@ class DotswanMap extends XotBasePage
         return 1;
     }
 
-    /**
-     * @return array<string, mixed>
-     */
     public function getFormSchema(): array
     {
         return [
