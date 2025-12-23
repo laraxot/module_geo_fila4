e5353186cb42f66af7bffff7dca8c3d31c6805d5
marco76tv <marco.sottana@gmail.com>
2025-12-23 13:06:24 +0100

    .

--- app/Filament/Resources/AddressResource/Pages/ListAddresses.php
+++ app/Filament/Resources/AddressResource/Pages/ListAddresses.php
@@ -8,6 +8,7 @@ use Filament\Actions\Action;
 use Filament\Actions\CreateAction;
 use Modules\Geo\Filament\Resources\AddressResource;
 use Modules\Xot\Filament\Resources\Pages\XotBaseListRecords;
+use Override;
 
 class ListAddresses extends XotBaseListRecords
 {
